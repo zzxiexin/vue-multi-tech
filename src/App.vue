4c1428b4b8b525040c5cdeb800da350813222551
yuanxinxin <yuanxinxin@111.com.cn>
2020-10-09 17:50:39 +0800

    '防抖'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <input type="text" @keyup="search" v-model="searchParams.keyword" />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      searchParams: {
+        keyword: "",
+        timer: null
+      }
+    };
+  },
+  methods: {
+    search() {
+      this.debounce(this.output, 1000)();
+    },
+    debounce(fn, delay) {
+      clearTimeout(this.timer);
+      return () => {
+        this.timer = setTimeout(() => {
+          fn.apply(this);
+        }, delay);
+      };
+    },
+    output() {
+      console.log("debounce success");
+    }
+  }
+};
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
