96d7997dd96f6e214fe11b4c387fce5093b401d0
yuanxinxin <yuanxinxin@111.com.cn>
2020-12-30 15:58:34 +0800

    '$attrs组件传值'

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <A name="xinxin" age="28" sex="male"></A>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-
+Vue.component("B", {
+  template: `<div>{{$attrs}}</div>`,
+  created() {
+    console.log(this.$attrs);
+  }
+});
+Vue.component("A", {
+  template: `<B v-bind="$attrs"></B>`
+});
 Vue.config.productionTip = false;
 
 new Vue({
